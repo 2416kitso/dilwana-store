87706684cb1d5e39ab031375e1cdc26d9db9ca28
MAITUMELO RADIPODI <mradipodi@gmail.com>
2023-08-12 22:01:36 +0200

    form styling

---
+++ dilwana-store/src/components/Footer.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Footer.css'; 
+import logo192 from '../assets/logo192.png';
+
+interface FooterProps {
+  companyName: string;
+}
+
+const Footer: React.FC<FooterProps> = ({ companyName }) => {
+  return (
+    <footer className="footer">
+       <div className="footer-logo">
+          <img className='logo-image'   src={logo192} alt="Company Logo" />
+          <div className="logo-text">
+            <p>Dilwana Store</p>
+            <p>Ke dilwana  ke dilwna 
+            hdfkjdfksjdkljkhflqjedjlf</p>
+          </div>
+        </div>
+        <div className="footer-form">
+            <p>Join Our News Collection</p>
+            <div className='form'>
+            <form>
+               
+            <div className='new'>
+            <label htmlFor="email"></label>
+            <input type="text" placeholder="Enter your email" id="email" name="email" />
+            </div>
+            
+            </form> 
+            </div>
+            
+        </div>
+        <div className="footer-info">
+            <p>hghjgfsdhjfgdkhsghkd</p>
+          <p>&copy; {new Date().getFullYear()} {companyName}. All rights reserved.</p>
+        </div>
+
+    </footer>
+  );
+};
+
+export default Footer;
